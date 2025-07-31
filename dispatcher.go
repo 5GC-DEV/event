ad65d513d5546e2c5b12c27ea0e6d384e145ddbc
anaswara <anaswara.n@cdac.in>
2025-07-31 13:03:06 +0530

    channel buffer size set to 1

--- dispatcher.go
+++ dispatcher.go
@@ -11,7 +11,7 @@ type Dispatcher struct {
 
 func NewDispatcher() *Dispatcher {
 	d := &Dispatcher{
-		jobs:   make(chan job, 100),
+		jobs:   make(chan job, 1),
 		events: make(map[Name]Listener),
 	}
 
@@ -34,25 +34,32 @@ func (d *Dispatcher) Register(listener Listener, names ...Name) error {
 
 func (d *Dispatcher) Dispatch(name Name, event interface{}) error {
 	fmt.Printf("*********** Dispatching Event Name: %s, Event Value: %#v\n", name, event)
+
 	if _, ok := d.events[name]; !ok {
 		return fmt.Errorf("the '%s' event is not registered", name)
 	}
-	fmt.Printf("*************** Before Job assign Event Name: %s", name)
-	d.jobs <- job{eventName: name, eventType: event}
-	fmt.Printf("Dispatched event: %s", name)
 
+	// Print channel occupancy and capacity
+	fmt.Printf("*************** Before assign Job Event Name: %s\n", name)
+	fmt.Println("Channel occupancy:", len(d.jobs))
+	fmt.Println("Channel capacity:", cap(d.jobs))
+
+	// Send job to the channel
+	d.jobs <- job{eventName: name, eventType: event}
+
+	fmt.Printf("Dispatched event: %s\n", name)
 	return nil
 }
 
-/*func (d *Dispatcher) consume() {
+func (d *Dispatcher) consume() {
 	fmt.Println("[DISPATCHER] consume loop started")
 	for job := range d.jobs {
 		fmt.Printf("Consuming event: %s", job.eventName)
 		d.events[job.eventName].Listen(job.eventType)
 	}
-}*/
+}
 
-func (d *Dispatcher) consume() {
+/*func (d *Dispatcher) consume() {
 	fmt.Println("[DISPATCHER] consume loop started")
 	for job := range d.jobs {
 		func() {
@@ -71,3 +78,4 @@ func (d *Dispatcher) consume() {
 		}()
 	}
 }
+*/
