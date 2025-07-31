dd0f12fa04b7bd01fbdbbd54182a72df83f0c75b
anaswara <anaswara.n@cdac.in>
2025-07-31 09:45:59 +0530

    logs added

--- dispatcher.go
+++ dispatcher.go
@@ -2,6 +2,7 @@ package event
 
 import (
 	"fmt"
+	"log"
 )
 
 type Dispatcher struct {
@@ -16,19 +17,19 @@ func NewDispatcher() *Dispatcher {
 	}
 
 	go d.consume()
-
+	log.Println("Dispatcher initialized")
 	return d
 }
 
 func (d *Dispatcher) Register(listener Listener, names ...Name) error {
 	for _, name := range names {
 		if _, ok := d.events[name]; ok {
+			log.Printf("Event '%s' already registered", name)
 			return fmt.Errorf("the '%s' event is already registered", name)
 		}
-
 		d.events[name] = listener
+		log.Printf("Registered event: %s", name)
 	}
-
 	return nil
 }
 
@@ -38,12 +39,14 @@ func (d *Dispatcher) Dispatch(name Name, event interface{}) error {
 	}
 
 	d.jobs <- job{eventName: name, eventType: event}
+	log.Printf("Dispatched event: %s", name)
 
 	return nil
 }
 
 func (d *Dispatcher) consume() {
 	for job := range d.jobs {
+		log.Printf("Consuming event: %s", job.eventName)
 		d.events[job.eventName].Listen(job.eventType)
 	}
 }
