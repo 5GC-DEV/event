3d7f9c51c82a073f4b9038ee632f2b27d60a8923
Anaswara Nambiar <159991511+anaswarac-dac@users.noreply.github.com>
2025-09-20 20:04:56 +0530

    Update dispatcher.go

--- dispatcher.go
+++ dispatcher.go
@@ -23,27 +23,6 @@ func NewDispatcher() *Dispatcher {
 	return d
 }
 
-/*type Dispatcher struct {
-	jobs        chan job
-	events      map[Name]Listener
-	workerCount int
-}
-
-func NewDispatcher(workerCount int, jobQueueSize int) *Dispatcher {
-	d := &Dispatcher{
-		jobs:        make(chan job, jobQueueSize),
-		events:      make(map[Name]Listener),
-		workerCount: workerCount,
-	}
-
-	for i := 0; i < d.workerCount; i++ {
-		go d.worker(i)
-	}
-
-	fmt.Println("Dispatcher initialized with", workerCount, "workers")
-	return d
-}*/
-
 func (d *Dispatcher) Register(listener Listener, names ...Name) error {
 	for _, name := range names {
 		if _, ok := d.events[name]; ok {
@@ -74,44 +53,6 @@ func (d *Dispatcher) Dispatch(name Name, event interface{}) error {
 	fmt.Printf("Dispatched event: %s\n", name)
 	return nil
 }
-
-/*func (d *Dispatcher) worker(id int) {
-	fmt.Printf("[WORKER %d] started\n", id)
-	for job := range d.jobs {
-		fmt.Printf("[WORKER %d] Processing event: %s\n", id, job.eventName)
-		d.events[job.eventName].Listen(job.eventType)
-	}
-}*/
-
-/*func (d *Dispatcher) consume() {
-	fmt.Println("[DISPATCHER] consume loop started")
-	for job := range d.jobs {
-		fmt.Printf("Consuming event: %s", job.eventName)
-		d.events[job.eventName].Listen(job.eventType)
-	}
-	fmt.Println("consume End")
-} */
-
-/*func (d *Dispatcher) consume() {
-	fmt.Println("[DISPATCHER] consume loop started")
-	for job := range d.jobs {
-		func() {
-			defer func() {
-				if r := recover(); r != nil {
-					fmt.Printf("[PANIC] while consuming event %s: %v\n", job.eventName, r)
-				}
-			}()
-			fmt.Printf("Consuming event: %s, Value: %#v\n", job.eventName, job.eventType)
-			listener, ok := d.events[job.eventName]
-			if !ok {
-				fmt.Printf("[ERROR] No listener for event: %s\n", job.eventName)
-				return
-			}
-			listener.Listen(job.eventType)
-		}()
-	}
-}
-*/
 
 func (d *Dispatcher) consume() {
 	fmt.Println("[DISPATCHER] consume loop started")
